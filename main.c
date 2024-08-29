c0c229c912803856ab2ba3773a7a8bfbf3ccc8e5
Ingrid Lopez <ingrdlezcano24@gmail.com>
2024-08-29 12:58:27 -0400

    Se hacen los cambios solicitados

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 //#include<stdlib.h>
 
 int main ( ) {
-    printf("Hola mundo\n");
-    printf("Hoy será un buen día\n"); //Se agrega una linea de codigo
+    printf("Hola, mundo.\n");
+    printf("Hoy será un buen día!!\n"); //Se agrega una linea de codigo
     return 0;
 
 }

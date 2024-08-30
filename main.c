1af2acc4bb118a4a5d0b343834a356e8f2cbcc0d
Diego Alcaraz <diego07alcrz@fpuna.edu.py>
2024-08-30 13:46:06 -0400

    Se agrega un nuevo mensaje

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 int main ( ) {
     printf("Hola, mundo.\n");
     printf("Hoy será un buen día!!\n"); //Se agrega una linea de codigo
+    printf("La entrega del trabajo sera un exito");
     return 0;
 
 }
